affa068e5fbfa364f82895ad806b9513207499d7
MBisovskyi <mbisovskyi@gmail.com>
2022-09-28 11:16:59 -0400

    test: switching back to the origin RDS url path

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://cors-anywhere.herokuapp.com/http://mlbackend-env.eba-x4jcnfu2.us-east-1.elasticbeanstalk.com"
+    ? "https://cors-anywhere.herokuapp.com/http://musiclibrarydatabase.cdg4nlqfn4tx.us-east-2.rds.amazonaws.com"
     : "http://127.0.0.1:8000";
